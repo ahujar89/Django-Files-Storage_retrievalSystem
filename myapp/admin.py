f9093e561ce8fd7ef5785efc49de06a943f339fc
Rishabh Ahuja <ahujar96@gmail.com>
2025-02-10 00:08:25 -0500

    [commit-4] Removed redundant package import

--- myapp/admin.py
+++ myapp/admin.py
@@ -6,6 +6,6 @@ admin.site.register(Product)
 admin.site.register(Category)
 admin.site.register(Client)
 admin.site.register(Order)
-from django.contrib import admin
+
 
 # Register your models here.
